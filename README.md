1358ed3b8ed3111ea47c8fcaffb241edba476720
Devmike01 <michaelgbenga91@gmail.com>
2023-08-18 21:55:53 +0100

    Changed version code

--- README.md
+++ README.md
@@ -9,3 +9,9 @@
         )
     }
 ```
+
+#### Add these dependencies to your project
+```kotlin
+implementation("com.github.devmike01.Chaicup:annotations:437079d4cc")
+ksp("com.github.devmike01.Chaicup:compiler:437079d4cc")
+```
--- compiler/build.gradle.kts
+++ compiler/build.gradle.kts
@@ -16,7 +16,7 @@ afterEvaluate {
             create<MavenPublication>("maven"){
                 groupId = "io.devmike01.chaicup"
                 artifactId = "compiler"
-                version = "1.1.0"
+                version = "1.2.0"
                 from(components["java"])
             }
 
